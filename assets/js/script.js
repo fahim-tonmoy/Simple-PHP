a25ed0739e8b2123107dce0fb8fd8b105b5566f6
Fahim Hasan <hasan.fahiim@gmail.com>
2024-02-07 15:10:10 +0600

    done

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let links = document.querySelectorAll(".delete");
-
-    for (let i = 0; i < links.length; i++) {
-        links[i].addEventListener("click", (e) => {
-            if(!confirm("Are you sure?")) {
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('loaded');
+    var links = document.querySelectorAll(".delete");
+    for(i=0;i<links.length;i++){
+        links[i].addEventListener('click',function(e){
+            if(!confirm("Are you sure?")){
                 e.preventDefault();
             }
-        })
-        
+        });
     }
 });
